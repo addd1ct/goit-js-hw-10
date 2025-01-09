d7b169dabd588d69efd516c37929ba1798d30149
polly <polifibliss@gmail.com>
2025-01-09 20:08:30 +0200

    update

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -19,22 +19,12 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    const selectedDate = selectedDates[0];
-
-    if (selectedDate <= new Date()) {
-      iziToast.error({
-        title: "Error",
-        message: "Please choose a date in the future",
-      });
-      startButton.disabled = true;
-    } else {
-      userSelectedDate = selectedDate;
-      startButton.disabled = false;
-    }
+    console.log(selectedDates[0]);
   },
 };
 
-flatpickr(".datetime-picker", options);
+flatpickr("#datetime-picker", options);
+
 
 function addLeadingZero(value) {
   return String(value).padStart(2, "0");
